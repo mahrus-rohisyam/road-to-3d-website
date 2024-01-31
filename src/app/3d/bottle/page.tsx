999045e80fc3d5fd7cbc437084a732ec10129624
mahrus-rohisyam <mahrusaim@gmail.comm>
2024-01-31 16:06:46 +0700

    Initial Project # Adding 3D Bottle Spline

---
+++ src/app/3d/bottle/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function Bottle() {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/PbGPp-Z9cIDUe4Eq/scene.splinecode" />
+    </div>
+  );
+}
---
+++ src/app/3d/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Button } from "@nextui-org/button";
+
+const index = () => {
+  return (
+    <>
+      <h1 className="sr-only">3D Website - Mahrus Rohisyam</h1>
+      <p>3D Website Showcase</p>
+
+      <div>
+        <ul>
+          <li>Test</li>
+          <Button>Click Me</Button>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default index;
